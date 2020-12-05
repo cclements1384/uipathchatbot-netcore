5b30d9d41301344eac48c34f957c387d18e2d4db
Chris Clements <cclements1384@gmail.com>
2020-12-05 16:45:30 -0600

    Added token provider interface and class.

---
+++ UiPath.API/Providers/TokenProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UiPath.API.Services
+{
+    public class TokenProvider : IUiPathTokenService
+    {
+        private readonly string _username;
+        private readonly string _password;
+        private readonly string _tenant;
+        const string _url = "https://cop-uipath-rpa-d.copapps.net";
+
+        public TokenProvider(string username, string password, string tenant)
+        {
+            _username = username;
+            _password = password;
+            _tenant = tenant;
+        }
+
+        public string GetToken()
+        {
+            var token = string.Empty;
+            return token;
+        }
+    }
+}
